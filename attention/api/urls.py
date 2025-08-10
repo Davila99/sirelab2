8a267232f0f67905e5ce2a7635d771e3558c3bbf
eliseo <davilaeliseo453@gmail.com>
2025-08-10 16:30:58 -0600

    add views,urls,serializers

---
+++ attention/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    AreaBeneficiariaViewSet,
+    TipoIncidenciaViewSet,
+    TipoEquipoRecursoViewSet,
+    AtencionIncidenciaViewSet
+)
+
+router = DefaultRouter()
+router.register(r'areas-beneficiarias', AreaBeneficiariaViewSet)
+router.register(r'tipos-incidencia', TipoIncidenciaViewSet)
+router.register(r'tipos-equipo-recurso', TipoEquipoRecursoViewSet)
+router.register(r'atenciones-incidencias', AtencionIncidenciaViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
