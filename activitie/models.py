50f546c3b593c2f8f975e1151fa4a5f872dbcd47
eliseo <davilaeliseo453@gmail.com>
2025-08-10 16:05:21 -0600

    creando modelos

--- activitie/models.py
+++ activitie/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class TipoActividad(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class EventoSoporteTecnico(models.Model):
+    tipo_actividad = models.ForeignKey(TipoActividad, on_delete=models.CASCADE, related_name='eventos')
+    nombre_evento = models.CharField(max_length=200)
+    fecha = models.DateField()
+    descripcion_asistencia = models.TextField()
+
+    def __str__(self):
+        return f"{self.nombre_evento} ({self.fecha})"
