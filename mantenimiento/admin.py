e3d4604a9a3bf705e3880790fb61142c66d48c6c
eliseo <davilaeliseo453@gmail.com>
2025-09-01 17:46:22 -0600

    Update admin.py

--- mantenimiento/admin.py
+++ mantenimiento/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Mantenimiento, AreaBeneficiaria, TipoMantenimiento, Equipo
 
-# Register your models here.
+
+@admin.register(Mantenimiento)
+class MantenimientoAdmin(admin.ModelAdmin):
+    list_display = ("area_beneficiario", "tipo_mantenimiento", "equipo", "cantidad", "fecha_registro")
+    search_fields = (
+        "area_beneficiario__nombre",
+        "tipo_mantenimiento__nombre",
+        "equipo__nombre",
+    )
+    list_filter = ("tipo_mantenimiento", "area_beneficiario", "equipo", "fecha_registro")
+    date_hierarchy = "fecha_registro"
+
+
+@admin.register(AreaBeneficiaria)
+class AreaBeneficiariaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre")
+    search_fields = ("nombre",)
+
+
+@admin.register(TipoMantenimiento)
+class TipoMantenimientoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre")
+    search_fields = ("nombre",)
+
+
+@admin.register(Equipo)
+class EquipoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre")
+    search_fields = ("nombre",)
