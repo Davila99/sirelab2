26a9b0c31e52c09d2df2fa3a1fdbbb4a8366ff98
eliseo <davilaeliseo453@gmail.com>
2025-09-01 18:45:24 -0600

    integrando decoradores
    integrando decoradores en admin de la app atettion

--- attention/admin.py
+++ attention/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
+from .models import AreaBeneficiaria, AtencionIncidencia, TipoEquipoRecurso, TipoIncidencia
 
-from attention.models import AreaBeneficiaria, AtencionIncidencia, TipoEquipoRecurso, TipoIncidencia
+# ------------------------------
+# Admin para AtencionIncidencia
+# ------------------------------
+@admin.register(AtencionIncidencia)
+class AtencionIncidenciaAdmin(admin.ModelAdmin):
+    list_display = ("area_beneficiaria", "tipo_incidencia", "tipo_equipo_recurso", "estado", "fecha_creacion")
+    search_fields = (
+        "area_beneficiaria__nombre",
+        "tipo_incidencia__descripcion",
+        "tipo_equipo_recurso__descripcion",
+        "actividad_realizada",
+    )
+    list_filter = ("estado", "area_beneficiaria", "tipo_incidencia", "tipo_equipo_recurso", "fecha_creacion")
+    date_hierarchy = "fecha_creacion"
 
-admin.site.register(AreaBeneficiaria)
-admin.site.register(AtencionIncidencia)
-admin.site.register(TipoEquipoRecurso)
-admin.site.register(TipoIncidencia)
+# ------------------------------
+# Admin para AreaBeneficiaria
+# ------------------------------
+@admin.register(AreaBeneficiaria)
+class AreaBeneficiariaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre")
+    search_fields = ("nombre",)
+
+# ------------------------------
+# Admin para TipoIncidencia
+# ------------------------------
+@admin.register(TipoIncidencia)
+class TipoIncidenciaAdmin(admin.ModelAdmin):
+    list_display = ("id", "descripcion")
+    search_fields = ("descripcion",)
+
+# ------------------------------
+# Admin para TipoEquipoRecurso
+# ------------------------------
+@admin.register(TipoEquipoRecurso)
+class TipoEquipoRecursoAdmin(admin.ModelAdmin):
+    list_display = ("id", "descripcion")
+    search_fields = ("descripcion",)
