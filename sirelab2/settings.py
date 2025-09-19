f4a0b7b4a60c9055e53745f5db5958955c15b084
Davila99 <davilaeliseo453@gmail.com>
2025-09-19 15:48:01 -0600

    Allow all hosts in ALLOWED_HOSTS
    Changed ALLOWED_HOSTS to ['*'] in settings.py to permit requests from any host. This is typically used for development or testing but should be restricted in production for security reasons.

--- sirelab2/settings.py
+++ sirelab2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ikl57=_8fjo_#82!_58$myhf76nb_s!%)v04=u4tgssm^1!^@^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
